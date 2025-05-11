6e80d6c795c6f2ad3e49303d561ab99bb0cab982
mc709 <mc709@hanmail.net>
2025-05-11 10:23:53 +0900

    Init 01

--- src/main/java/com/madang/mavenproj/App.java
+++ src/main/java/com/madang/mavenproj/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" ); //02
+        System.out.println( "Hello World!" ); 
     } 
 } 
